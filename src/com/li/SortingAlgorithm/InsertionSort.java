82e2f8c205ef14f801b676631971278a555610ee
CloverLily <lily_clover6@163.com>
2018-10-08 17:30:01 +0800

    new -> src/com/li/SortingAlgorithm/InsertionSort/ShellSort

--- src/com/li/SortingAlgorithm/InsertionSort.java
+++ src/com/li/SortingAlgorithm/InsertionSort.java
@@ -6,7 +6,9 @@ package com.li.SortingAlgorithm;
  */
 public class InsertionSort {
 
-    public static void insertionSort(int[] arr) {
+    public static final int[] arr = {3, 2, 6, 4, 7, 8, 1, 0, 9, 5};
+
+    private static void insertionSort(int[] arr) {
         int len = arr.length;
 
         for (int i = 1; i < len; i++) {//类似抓扑克牌排序
@@ -29,8 +31,7 @@ public class InsertionSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3, 2, 6, 4, 7, 8, 1, 0, 9, 5};
-        insertionSort(arr);
+        insertionSort(InsertionSort.arr);
     }
 }
 
@@ -39,7 +40,7 @@ public class InsertionSort {
  * 二分插入排序
  */
 class BinaryInsertionSort {
-    public static void binaryInsertionSort(int[] arr) {
+    private static void binaryInsertionSort(int[] arr) {
         int len = arr.length;
 
         for (int i = 1; i < len; i++) {
@@ -49,7 +50,7 @@ class BinaryInsertionSort {
             int left = 0;
             //手牌左右边界进行初始化
             int right = i - 1;
-            while (left <= right) {
+            while (left <= right) {// 采用二分法定位新牌的位置:left
                 int mid = (left + right) / 2;
                 if (arr[mid] > get)
                     right = mid - 1;
@@ -70,7 +71,53 @@ class BinaryInsertionSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3, 2, 6, 4, 7, 8, 1, 0, 9, 5};
-        binaryInsertionSort(arr);
+        binaryInsertionSort(InsertionSort.arr);
+    }
+}
+
+/**
+ * 插入排序的更高效改进：
+ * 希尔排序(Shell Sort)
+ */
+class ShellSort {
+    /**
+     * 分类 -------------- 内部比较排序
+     * 数据结构 ---------- 数组
+     * 最差时间复杂度 ---- 根据步长序列的不同而不同。已知最好的为O(n(logn)^2)
+     * 最优时间复杂度 ---- O(n)
+     * 平均时间复杂度 ---- 根据步长序列的不同而不同。
+     * 所需辅助空间 ------ O(1)
+     * 稳定性 ------------ 不稳定
+     */
+
+    private static void shellSort(int[] arr) {
+        int len = arr.length;
+        int h = 0;
+
+        while (h <= len) {//生成初始增量
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {//插入排序
+            for (int i = h; i < len; i++) {
+                int j = i - h;//从右向左比较
+                int get = arr[i];//拿到一张牌
+                while (j >= 0 && arr[j] > get) {
+                    arr[j + h] = arr[j];
+                    j = j - h;//依次与向左相隔距离h的数值进行比较
+                }
+                arr[j + h] = get;
+            }
+            h = (h - 1) / 3;//递减增量
+        }
+
+        System.out.println("希尔排序结果");
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        shellSort(InsertionSort.arr);
     }
 }
