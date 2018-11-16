a4b8d321c067a9ba0a8e9a0f831e5fc591d8da97
Clover_lily <lily_clover6@163.com>
2018-11-16 15:09:31 +0800

    update

--- src/com/li/Easy/_7ReverseInteger.java
+++ src/com/li/Easy/_7ReverseInteger.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * 7 反转整数
- * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+ * 给定一个32位有符号整数，将整数中的数字进行反转。
  */
 class _7ReverseInteger {
     private static int reverse(int x) {
@@ -22,7 +22,9 @@ class _7ReverseInteger {
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse(123));
-        List<List> qList = new ArrayList<>();
+        //翻转后正常
+        System.out.println(reverse(123456789));
+        //翻转后溢出
+        System.out.println(reverse(1999999999));
     }
 }
