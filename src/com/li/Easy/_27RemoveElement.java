badee968f671f49bb62f6e8e5b7a3805ef964a18
Clover_lily <lily_clover6@163.com>
2018-11-23 22:16:13 +0800

    update 27

--- src/com/li/Easy/_27RemoveElement.java
+++ src/com/li/Easy/_27RemoveElement.java
@@ -2,12 +2,12 @@ package com.li.Easy;
 
 /**
  * 27. 移除元素
- * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * 给定一个数组nums和一个值val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
  * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
  * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
- *
  */
 public class _27RemoveElement {
+    //如果存在等于val的值，则用末端的数值覆盖该值
     public static int removeElement(int[] nums, int val) {
         int len = nums.length;
         if (nums == null || len == 0) return 0;
@@ -24,6 +24,10 @@ public class _27RemoveElement {
 
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
-        System.out.println(removeElement(nums, 2));
+        int len = removeElement(nums, 2);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i]+",");
+        }
     }
 }
