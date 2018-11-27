a54a80ca5f31cf3f258ed5fe73cbd48746b27621
Clover_lily <lily_clover6@163.com>
2018-11-27 22:20:57 +0800

    update 38

--- src/com/li/Easy/_38CountAndSay.java
+++ src/com/li/Easy/_38CountAndSay.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * 38 报数
  * 报数序列是指一个整照其中的整数的顺序进数序列，按行报数，得到下一个数。其前五项如下：
- *
+ * <p>
  * 1.     1
  * 2.     11
  * 3.     21
@@ -15,55 +15,71 @@ import java.util.List;
  * 1 被读作  "one 1"  ("一个一") , 即 11。
  * 11 被读作 "two 1s" ("两个一"）, 即 21。
  * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
- *
+ * <p>
  * 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
- *
+ * <p>
  * 注意：整数顺序将表示为一个字符串。
- *
  */
 public class _38CountAndSay {
-    public static String[] strArr = new String[30];
+
+    private static String[] countArr = new String[30];
+
 
     public static void main(String[] args) {
-        strArr[0] = "1";
-        strArr[1] = "11";
-        System.out.println(countAndSay(9));
+        System.out.println(countAndSay(5));
     }
 
     public static String countAndSay(int n) {
+        countArr[0] = "1";
+        countArr[1] = "11";
 
-        while (strArr[n - 1] == null) {
-            if (strArr[n - 2] != null) {
-                strArr[n - 1] = count(strArr[n - 2]);
+        if (n == 1) {
+            return "1";
+        } else if (n == 2) {
+            return "11";
+        }
+
+        return getCount(n);
+    }
+
+    private static String getCount(int n) {
+        while (countArr[n - 1] == null) {
+            if (countArr[n - 2] != null) {
+                countArr[n - 1] = count(countArr[n - 2]);
             } else {
-                strArr[n - 2] = countAndSay(n - 1);
+                countArr[n - 2] = getCount(n - 1);
             }
         }
-        return strArr[n - 1];
+
+        return countArr[n - 1];
     }
 
     private static String count(String s) {
         char[] sChar = s.toCharArray();
-        //存放字符
-        List<Character> charList = new ArrayList<>();
-        List<Integer> countList = new ArrayList<>();
-        for (int i = 0; i < sChar.length; i++) {
-            int index = charList.indexOf(sChar[i]);
-            if (index != -1) {
-                int val = countList.get(index);
-                countList.set(index, val + 1);
+        StringBuffer countStr = new StringBuffer();
+
+        int i = 0;
+        int j = i + 1;
+        int count = 1;
+        while (i < sChar.length) {
+            if (i == sChar.length - 1) {
+                countStr.append(count);
+                countStr.append(sChar[i]);
+            } else if (j<sChar.length && sChar[i] == sChar[j]) {
+                count++;
+                j++;//有问题，走到头需要计数
+                if (j >= sChar.length) {
+                    countStr.append(count);
+                    countStr.append(sChar[i]);
+                }
             } else {
-                charList.add(sChar[i]);
-                countList.add(1);
+                countStr.append(count);
+                countStr.append(sChar[i]);
+                count = 1;
+                i = j;
+                j = i + 1;
             }
         }
-
-        StringBuffer countSb = new StringBuffer();
-        for (int i = 0; i < charList.size(); i++) {
-            countSb.append(countList.get(i));
-            countSb.append(charList.get(i));
-        }
-
-        return countSb.toString();
+        return countStr.toString();
     }
 }
