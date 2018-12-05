8b6f162f1ba237837b5ad1e8b504aab7e5282321
Clover_lily <lily_clover6@163.com>
2018-12-05 18:00:21 +0800

    update 003

--- src/com/li/Medium/_003LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/li/Medium/_003LongestSubstringWithoutRepeatingCharacters.java
@@ -29,57 +29,65 @@ import java.util.Set;
 public class _003LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("tmmzuxt"));
+        System.out.println("pwke中最大的不重复子字符串共有:"+lengthOfLongestSubstring("pwke")+"个字符");
+        System.out.println("abcabcbb中最大的不重复子字符串共有:"+Solution.lengthOfLongestSubstring2("abcabcbb")+"个字符");
     }
 
     public static int lengthOfLongestSubstring(String s) {
         int n = s.length(), max = 0;
-        //字符-索引表
+        //字符-（索引+1）表
         Map<Character, Integer> strMap = new HashMap<>();
-        //开始创建窗口[i,j]
+        //创建窗口[i,j]
         for (int i = 0, j = 0; j < n; j++) {
-            //判断该字符是否已保存
+            //判断该字符是否已保存在表中
             if (strMap.containsKey(s.charAt(j))) {
-                //如果有，更新左区间的索引,即滑动窗口
+                //如果有且包含在目前的窗口中，则更新左区间的索引为该字符的索引+1,即向右滑动窗口左区间
                 i = Math.max(strMap.get(s.charAt(j)), i);
             }
-            max = Math.max(max, j - i + 1);
+            //保存（该字符，字符的下一个位置）
             strMap.put(s.charAt(j), j + 1);
+            max = Math.max(max, j - i + 1);
         }
         return max;
     }
 }
 
 /**
+ * 最优秀的解法
+ * 采用字符矩阵
+ */
+/**
  * Java（假设字符集为 ASCII 128）
- * <p>
  * 以前的我们都没有对字符串s所使用的字符集进行假设。
  * 当我们知道该字符集比较小的时侯，我们可以用一个整数数组作为直接访问表来替换 Map。
  * 常用的表如下所示：
- * <p>
  * int [26] 用于字母 ‘a’ - ‘z’或 ‘A’ - ‘Z’
  * int [128] 用于ASCII码
  * int [256] 用于扩展ASCII码
- * <p>
- * public class Solution {
- * public int lengthOfLongestSubstring(String s) {
- * int n = s.length(), ans = 0;
- * int[] index = new int[128]; // current index of character
- * // try to extend the range [i, j]
- * for (int j = 0, i = 0; j < n; j++) {
- * i = Math.max(index[s.charAt(j)], i);
- * ans = Math.max(ans, j - i + 1);
- * index[s.charAt(j)] = j + 1;
- * }
- * return ans;
- * }
- * }
- * <p>
+ *
  * 复杂度分析
- * <p>
  * 时间复杂度：O(n)，索引j将会迭代n次。
- * <p>
  * 空间复杂度（HashMap）：O(min(m, n))，与之前的方法相同。
- * <p>
  * 空间复杂度（Table）：O(m)，m 是字符集的大小。
  */
+ class Solution {
+    public static int lengthOfLongestSubstring2(String s) {
+        //字符串长度n，最长子字符串中的字符个数max
+        int n = s.length(), max = 0;
+        // 索引数组（存放字符目前的索引+1）
+        int[] index = new int[128];
+        // 创建窗口并滑动，最后锁定最长不重复子字符串[i,j]
+        for (int j = 0, i = 0; j < n; j++) {
+            //如果该字符未出现过，则不更新；
+            // 如果该字符已出现但在区间外，即比i小，也不更新；
+            // 如果该字符已出现过且在区间中，即比i大，则更新i；
+            i = Math.max(index[s.charAt(j)], i);
+            max = Math.max(max, j - i + 1);
+            //更新该字符数组为：目前的索引+1
+            index[s.charAt(j)] = j + 1;
+        }
+        return max;
+    }
+}
+
+
