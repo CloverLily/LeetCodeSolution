e6c87927002c1239b08c27e1ae2ca19c0608015b
Clover_lily <lily_clover6@163.com>
2018-12-14 22:03:18 +0800

    update 100

---
+++ src/com/li/Common/TreeNode.java
@@ -0,0 +1,15 @@
+package com.li.Common;
+
+/**
+ * Definition for a binary tree node.
+ * 树节点
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/li/Easy/_100SameTree.java
@@ -0,0 +1,10 @@
+package com.li.Easy;
+
+import com.li.Common.TreeNode;
+
+public class _100SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        return false;
+    }
+}
