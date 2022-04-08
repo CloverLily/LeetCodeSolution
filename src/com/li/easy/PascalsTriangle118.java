1960b437df82ad4a79693862a85a800e228dce3a
CloverLily <lily_clover6@163.com>
2022-04-08 23:23:36 +0800

    [resolve 118]

--- src/com/li/easy/PascalsTriangle118.java
+++ src/com/li/easy/PascalsTriangle118.java
@@ -1,5 +1,6 @@
 package com.li.easy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,11 +25,52 @@ import java.util.List;
 public class PascalsTriangle118 {
     public static void main(String[] args) {
 
+        List<List<Integer>> arrTriangle = generate(10);
+        for (List<Integer> row : arrTriangle) {
+            for (Integer num : row) {
+                System.out.print(num);
+                System.out.print(",");
+            }
+            System.out.println();
+        }
     }
-    public List<List<Integer>> generate(int numRows) {
 
+    /**
+     * f(x,y) = f(x-1,y-1) + f(x-1,y)
+     *
+     * @param numRows 行数
+     * @return 杨辉三角数组
+     */
+    public static List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) {
+            return new ArrayList<>();
+        }
 
-        return null;
+        List<List<Integer>> arrTriangle = new ArrayList<>(numRows);
 
+        int[][] rowArr = new int[numRows][numRows];
+        List<Integer> rowTriangle;
+        for (int x = 1; x <= numRows; x++) {
+            rowTriangle = new ArrayList<>(x);
+
+            rowArr[x - 1][0] = 1;
+            if (x != 1) {
+                rowArr[x - 1][x - 1] = 1;
+            }
+
+            for (int y = 2; (y - 1) <= (x / 2); y++) {
+                rowArr[x - 1][y - 1] = rowArr[x - 2][y - 2] + rowArr[x - 2][y - 1];
+                if (2 * y != x + 1) {
+                    rowArr[x - 1][x - y] = rowArr[x - 2][y - 2] + rowArr[x - 2][y - 1];
+                }
+            }
+
+            for (int i = 0; i < x; i++) {
+                rowTriangle.add(rowArr[x - 1][i]);
+            }
+            arrTriangle.add(rowTriangle);
+        }
+        return arrTriangle;
     }
+
 }
