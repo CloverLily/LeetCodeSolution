e6c1be8692931b8cba1d73fb69cfbf2dac33ae1a
CloverLily <lily_clover6@163.com>
2022-05-03 15:58:13 +0800

    [resolve 83]删除排序链表中的重复元素

--- src/com/li/easy/RemoveDuplicatesFromSortedList083.java
+++ src/com/li/easy/RemoveDuplicatesFromSortedList083.java
@@ -13,9 +13,15 @@ import com.li.common.ListNode;
  * 示例 2:
  * 输入: 1->1->2->3->3
  * 输出: 1->2->3
+ *
+ * 提示：
+ *     链表中节点数目在范围 [0, 300] 内
+ *     -100 <= Node.val <= 100
+ *     题目数据保证链表已经按升序 排列
+ *
  * @author system
  */
-public class RemoveDuplicatesFromSortedList_083 {
+public class RemoveDuplicatesFromSortedList083 {
 
     public static void main(String[] args) {
         //1->1->2->3->3
@@ -28,7 +34,7 @@ public class RemoveDuplicatesFromSortedList_083 {
         a2.next = a3;
         a3.next = a4;
         a4.next = a5;
-        l1 = deleteDuplicates(l1);
+        l1 = deleteDuplicates0503(l1);
         while (l1 != null) {
             //1->2->3
             System.out.print(l1.val);
@@ -42,7 +48,7 @@ public class RemoveDuplicatesFromSortedList_083 {
         ListNode b3 = new ListNode(1);
         l2.next = b2;
         b2.next = b3;
-        l2 = deleteDuplicates(l2);
+        l2 = deleteDuplicates0503(l2);
         while (l2 != null) {
             System.out.print(l2.val);
             l2 = l2.next;
@@ -50,20 +56,20 @@ public class RemoveDuplicatesFromSortedList_083 {
 
     }
 
-    public static ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
+    public static ListNode deleteDuplicates0503(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
 
-        ListNode temp = head;
-        while (temp.next != null) {
-            //如果值和下一个节点相等则删除下一个节点
-            if (temp.val == temp.next.val) {
-                temp.next = temp.next.next;
+        ListNode pre = head;
+        while (pre.next != null) {
+            if (pre.val == pre.next.val) {
+                pre.next = pre.next.next;
             } else {
-                //如果值不想等则指针继续往后走
-                // 指针所在的位置是处理后的节点（即该节点与前节点值不同），所以只需要比较指针所在位置和下一个节点的值
-                temp = temp.next;
+                pre = pre.next;
             }
         }
         return head;
     }
+
 }
